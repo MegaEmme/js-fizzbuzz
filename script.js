6ca6372c9cced4c9321ea353d7604c5668269143
MegaEmme <mikmusco@gmail.com>
2025-02-25 14:19:03 +0100

    feat: completed task

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Mike')
+console.log("Mike's FizzBuzz")
+
+for (let i=1 ; i <=100; i++) {
+    
+    if (i%3 === 0 && i%5 ===0) {
+        let i = 'FizzBuzz';
+        console.log(i);
+    }
+
+    else if (i%3 === 0) {
+        let i = 'Fizz';
+        console.log(i);       
+    }
+
+    else if (i%5 === 0) {
+        let i = 'Buzz';
+        console.log(i);
+    }
+
+    else {
+        console.log(i);
+    }
+
+}  
